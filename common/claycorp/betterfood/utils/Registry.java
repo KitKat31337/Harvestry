55808ff21fffd02419ecf03677bfaab7d836c3ca
Captain of all Shadows <captain_shadows@aol.com>
2013-04-10 00:00:47 -0400

    Stared World gen

--- common/claycorp/betterfood/utils/Registry.java
+++ common/claycorp/betterfood/utils/Registry.java
@@ -3,9 +3,12 @@ package claycorp.betterfood.utils;
 import claycorp.betterfood.blocks.ModBlocks;
 import claycorp.betterfood.items.ModItems;
 import claycorp.betterfood.utils.handlers.RecipeHandler;
+import claycorp.betterfood.utils.handlers.WorldGenHandler;
 import cpw.mods.fml.common.registry.GameRegistry;
 
 public class Registry {
+
+    private static WorldGenHandler worldGen = new WorldGenHandler();
     
     public static void register() {
 
@@ -13,11 +16,13 @@ public class Registry {
 
         registerItems();
 
+        registerWorld();
+
         RecipeHandler.init();
     }
 
     private static void registerBlocks() {
-        
+
         GameRegistry.registerBlock(ModBlocks.oreAluminum, Archive.oreAluminum);
     }
 
@@ -30,24 +35,28 @@ public class Registry {
         GameRegistry.registerItem(ModItems.itemCakeBatter, Archive.itemCakeBatter);
 
         GameRegistry.registerItem(ModItems.itemCakePan, Archive.itemCakePan);
-        
+
         GameRegistry.registerItem(ModItems.itemCakePanFull, Archive.itemCakePanFull);
-        
+
         GameRegistry.registerItem(ModItems.itemChocolateChips, Archive.itemChocolateChips);
-        
+
         GameRegistry.registerItem(ModItems.itemCookieSheet, Archive.itemCookieSheet);
-        
+
         GameRegistry.registerItem(ModItems.itemDough, Archive.itemDough);
-        
+
         GameRegistry.registerItem(ModItems.itemModBucket, Archive.itemModBucket);
-        
+
         GameRegistry.registerItem(ModItems.foodPeanuts, Archive.foodPeanuts);
-        
+
         GameRegistry.registerItem(ModItems.itemPiePan, Archive.itemPiePan);
-        
+
         GameRegistry.registerItem(ModItems.itemPumpkinMash, Archive.itemPumpkinMash);
-        
-        //GameRegistry.registerItem(ModItems.itemPoisonousPeanut, Archive.foodPeanuts + "Poison");
+
+        // GameRegistry.registerItem(ModItems.itemPoisonousPeanut,
+        // Archive.foodPeanuts + "Poison");
     }
 
+    private static void registerWorld() {
+        GameRegistry.registerWorldGenerator(worldGen);
+    }
 }
--- common/claycorp/betterfood/utils/handlers/WorldGenHandler.java
+++ common/claycorp/betterfood/utils/handlers/WorldGenHandler.java
@@ -1,5 +1,18 @@
 package claycorp.betterfood.utils.handlers;
 
-public class WorldGenHandler {
+import java.util.Random;
+
+import net.minecraft.world.World;
+import net.minecraft.world.chunk.IChunkProvider;
+import cpw.mods.fml.common.IWorldGenerator;
+
+public class WorldGenHandler implements IWorldGenerator{
+
+    @Override
+    public void generate(Random random, int chunkX, int chunkZ, World world,
+            IChunkProvider chunkGenerator, IChunkProvider chunkProvider) {
+        
+        
+    }
 
 }
